07dd0394e2ba0a17326dc319baa6f60b41c1c846
Caleb <ca30850@gmail.com>
2026-03-17 22:51:15 -0500

    fix(eslint): resolve type-aware linting for bullmq by configuring parserOptions.project

--- src/modules/game/game.module.ts
+++ src/modules/game/game.module.ts
@@ -6,9 +6,28 @@ import { CacheModule } from 'src/common/src/cache/cache.module';
 import { MatchModule } from './match/match.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ENTITIES } from 'src/db/database.module';
+import { BullModule } from '@nestjs/bullmq';
+import { EnvsModule } from 'src/common/src/envs/envs.module';
+import { EnvsService } from 'src/common/src/envs/envs.service';
 
 @Module({
   providers: [GameGateway, GameService],
-  imports: [QuestionModule, CacheModule, MatchModule, TypeOrmModule.forFeature(ENTITIES)],
+  imports: [
+    QuestionModule,
+    CacheModule,
+    MatchModule,
+    TypeOrmModule.forFeature(ENTITIES),
+    BullModule.registerQueueAsync({
+      name: 'timeout-question',
+      imports: [EnvsModule],
+      useFactory: async (envs: EnvsService) => ({
+        connection: {
+          host: envs.redisHost,
+          port: envs.redisPort,
+        },
+      }),
+      inject: [EnvsService],
+    }),
+  ],
 })
 export class GameModule {}
